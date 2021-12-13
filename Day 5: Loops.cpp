de934a13ba5d90d2c6880a0cf3e7a97349e92b2d
Swapnil Singhal <77186164+swapnil46@users.noreply.github.com>
2021-12-13 20:30:21 +0530

    Create Day 5: Loops.cpp

---
+++ Day 5: Loops.cpp
@@ -0,0 +1,19 @@
+#Day 5: Loops
+
+#Hackerrank problem solved using C++.
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cin>>n;
+    for (i = 1; i <= 10; ++i) 
+    {
+       cout << n << " x " << i << " = " << n * i << endl;
+    }
+  return 0;
+}
